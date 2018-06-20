42b5ab217a1dd3006e9a6d246a33b710b1068304
Junior Maleo <jmaleomi@my.centennialcollege.ca>
2018-06-20 12:33:31 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,35 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+(function () {
+"use strict";
+
+(function (app) {
+    "use strict"
+
+    // About Button Click event handler
+
+    function injecte() {
+        // local variable
+        let paragraphs = paragraphs.txt;
+
+        
+        console.log("Title: " + paragraphs);
+
+    }
+
+
+    //window.onload = Start;
+
+    window.addEventListener("load", inject);
+
+
+    // these are all app properties
+    app.paragraphs = paragraphs.txt;
+
+
+})(app || (app = {}));
+
+// YOUR CODE GOES HERE
+
+
+})();
+
